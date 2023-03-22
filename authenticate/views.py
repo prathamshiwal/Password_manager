eb74b0f3944f18613991ed08818ff5e93afea698
prathamshiwal <shiwalpratham2000@gmail.com>
2023-03-22 12:21:33 -0400

    signup and login work

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,15 +1,23 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
-def home(request):
-    return render(request, 'authenticate/index.html')
 
 def signup(request):
-    return render(request, 'authenticate/signup.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["Email"]
+        password = request.POST["Password"]
 
-def signin(request):
-    return render(request, 'authenticate/signin.html')
+        newUser = User.objects.create_user(username, email, password)
+        newUser.save()
+        messages.success(request, 'Account has been succesfully created')
 
-def signout(request):
-    pass
+        return redirect('signup')
+    
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'login.html')
