a474c9ed3267dfc5307a546502ad6f6e4ef33b6f
prathamshiwal <shiwalpratham2000@gmail.com>
2025-01-01 17:45:12 -0500

    scrap whole project to rebuild

--- authenticate/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('signup', views.signup_request, name="signup"),
-    path('login', views.login_request, name="login"),
-]
